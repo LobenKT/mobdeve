bbed0637aff23e5503999ebfa90f5814ea533eda
Gabe Piano <43084571+Poije@users.noreply.github.com>
2023-10-24 14:59:00 +0800

    Added Bottom Nav Bar and Profile

--- app/src/main/java/com/mobdeve/s16/mindpal/ProfileActivity.java
+++ app/src/main/java/com/mobdeve/s16/mindpal/ProfileActivity.java
@@ -4,33 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
-import com.google.android.material.navigation.NavigationView;
-import com.mobdeve.s16.mindpal.databinding.ActivityMainBinding;
 
-public class HomeActivity extends AppCompatActivity {
-
-    TextView WelcomeText;
+public class ProfileActivity extends AppCompatActivity {
 
     BottomNavigationView navView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
 
         navView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
 
-        WelcomeText = (TextView) findViewById(R.id.Welcome_Message);
-        String username = getIntent().getStringExtra("KeyUsername");
-        WelcomeText.setText("Welcome, " + username);
-
         navView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.Profile_btn){
-                Intent intent = new Intent(this, ProfileActivity.class);
+            if (item.getItemId() == R.id.Home_btn){
+                Intent intent = new Intent(this, HomeActivity.class);
                 startActivity(intent);
             }
             else if (item.getItemId() == R.id.Settings_btn){
