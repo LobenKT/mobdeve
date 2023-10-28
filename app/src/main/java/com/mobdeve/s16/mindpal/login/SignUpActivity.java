2f561b2b6a7be843c3d9f912782b628325a87f9f
lobentipan <loben_klien_tipan@dlsu.edu.ph>
2023-10-28 17:16:43 +0800

    Added Popup Messages
    added popup messages for successful login, signup, and goal added

--- app/src/main/java/com/mobdeve/s16/mindpal/login/SignUpActivity.java
+++ app/src/main/java/com/mobdeve/s16/mindpal/login/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.mobdeve.s16.mindpal.R;
 
@@ -23,6 +24,7 @@ public class SignUpActivity extends AppCompatActivity {
         SignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(SignUpActivity.this, "Account created, Please login with your credentials", Toast.LENGTH_SHORT).show();
                 openLoginActivity();
 
             }
