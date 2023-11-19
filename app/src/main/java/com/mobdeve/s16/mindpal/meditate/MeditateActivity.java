234be0ad9e7ad6ce4511d628f9cf3e84abab125f
Gabe Piano <43084571+Poije@users.noreply.github.com>
2023-11-19 21:33:53 +0800

    Added Youtube

--- app/src/main/java/com/mobdeve/s16/mindpal/meditate/MeditateActivity.java
+++ app/src/main/java/com/mobdeve/s16/mindpal/meditate/MeditateActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.mobdeve.s16.mindpal.NavigationActivity;
 import com.mobdeve.s16.mindpal.R;
+
+import com.androdocs.httprequest.HttpRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -20,8 +29,8 @@ public class MeditateActivity extends NavigationActivity {
     meditation_courses CourseHolder;
     RecyclerView meditation_recycler;
     meditation_Adaptor meditationAdaptor;
-
     Button sleep_btn , focus_btn, relax_btn, happy_btn, reset_btn;
+    String API = "AIzaSyDT3u9rFL-G4cQx_HHnzd_qDPc-2CA7XOo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +47,7 @@ public class MeditateActivity extends NavigationActivity {
         meditation_recycler = findViewById(R.id.meditate_recycler);
 
         // Test Values
-
+/*
         meditationCourses.add(new meditation_courses("Sleep", "Sleep Meditation", "You Feel Sleep now", R.drawable.sample_thumbnail));
         meditationCourses.add(new meditation_courses("Sleep", "Sleep Meditation2", "You Feel Sleep now fr", R.drawable.sample_thumbnail));
         meditationCourses.add(new meditation_courses("Focus", "Focus Meditation", "You Feel Focus now", R.drawable.sample_thumbnail));
@@ -47,7 +56,7 @@ public class MeditateActivity extends NavigationActivity {
         meditationCourses.add(new meditation_courses("Relax", "Relax Meditation", "You Feel Relax now fr", R.drawable.sample_thumbnail));
         meditationCourses.add(new meditation_courses("Happiness", "Happy Meditation", "You Feel Happy now", R.drawable.sample_thumbnail));
         meditationCourses.add(new meditation_courses("Happiness", "Happy Meditation", "You Feel Happy now fr", R.drawable.sample_thumbnail));
-
+*/
         meditationAdaptor = new meditation_Adaptor(this, meditationCourses);
         meditation_recycler.setAdapter(meditationAdaptor);
         meditation_recycler.setLayoutManager(new LinearLayoutManager(this));
@@ -87,6 +96,37 @@ public class MeditateActivity extends NavigationActivity {
             }
         });
 
+        new YoutubeVideo().execute();
+    }
+
+    class YoutubeVideo extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = HttpRequest.excuteGet("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&key=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONArray item = jsonObj.getJSONArray("items");
+                for (int i = 0; i < item.length() ; i++) {
+                    JSONObject JSONObject1= item.getJSONObject(i);
+                    JSONObject jsonSnippet = JSONObject1.getJSONObject("snippet");
+                    String videoTitle = jsonSnippet.getString("title");
+                    String videoThumbnail = jsonSnippet.getJSONObject("thumbnails").getJSONObject("medium").getString("url");
+                    meditationCourses.add(new meditation_courses("Sleep", videoTitle, "You Feel Sleep now", videoThumbnail));
+                    Log.d("TAG", "onPostExecute: videoTitle " + videoTitle);
+                }
+                meditation_recycler.setAdapter(meditationAdaptor);
+                meditation_recycler.setLayoutManager(new LinearLayoutManager(MeditateActivity.this));
+                //Toast.makeText(MeditateActivity.this, "Video Title: " + videoTitle, Toast.LENGTH_LONG).show();
+            }catch (JSONException e){
+                Toast.makeText(MeditateActivity.this, "Sira Pre", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     public void SleepCourse(ArrayList<meditation_courses> meditationCourses){
