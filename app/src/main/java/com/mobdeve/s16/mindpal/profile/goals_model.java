9c861efcdfc33d47c82d2af18b85c01474e8eb0f
Gabe Piano <43084571+Poije@users.noreply.github.com>
2023-12-02 13:59:01 +0800

    Add Goals to DB and Fetch

--- app/src/main/java/com/mobdeve/s16/mindpal/profile/goals_model.java
+++ app/src/main/java/com/mobdeve/s16/mindpal/profile/goals_model.java
@@ -1,27 +1,37 @@
 package com.mobdeve.s16.mindpal.profile;
 
 public class goals_model {
+    int id;
     String GoalTitle;
     String CurrentProgress;
 
-    public goals_model(String goalTitle, String currentProgress) {
+    public goals_model(int id, String goalTitle, String currentProgress) {
+        this.id = id;
         GoalTitle = goalTitle;
         CurrentProgress = currentProgress;
     }
-    public String getGoalTitle() {
-        return GoalTitle;
+
+    public int getId() {
+        return id;
     }
 
-    public String getCurrentProgress() {
-        return CurrentProgress;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGoalTitle() {
+        return GoalTitle;
     }
 
     public void setGoalTitle(String goalTitle) {
         GoalTitle = goalTitle;
     }
 
+    public String getCurrentProgress() {
+        return CurrentProgress;
+    }
+
     public void setCurrentProgress(String currentProgress) {
         CurrentProgress = currentProgress;
     }
-
 }
